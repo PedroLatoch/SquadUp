07970bac42b00a66fa645beb482f58129d5f3d15
Pedro Henrique Hahn Latoch <pedro.latoch@pucpr.edu.br>
2026-08-26 19:23:00 -0300

    colocando o modal

--- js/registro/info_usuarios.js
+++ js/registro/info_usuarios.js
@@ -3,6 +3,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirm_password = document.getElementById('confirm-password');
 const register_button = document.getElementById('register-button');
+const login_button = document.getElementById('login_button')
 
 register_button.addEventListener('click', function(event) {
     event.preventDefault();
@@ -13,7 +14,7 @@ register_button.addEventListener('click', function(event) {
     const confirmPasswordValue = confirm_password.value.trim();
 
     if (usernameValue === '' || emailValue === '' || passwordValue === '' || confirmPasswordValue === '') {
-        alert('Por favor, preencha todos os campos.');
+        Modal_Registro_sem_campos()
         return;
     }
     if (passwordValue !== confirmPasswordValue) {
@@ -25,3 +26,36 @@ register_button.addEventListener('click', function(event) {
     console.log("Nome:", usernameValue);
     console.log("E-mail:", emailValue);
 });
+login_button.addEventListener('click', function(event){
+ event.preventDefault();
+
+
+})
+function Modal_Registro_sem_campos() {
+    const modal = document.getElementById("modal_sem_usuario");
+
+    modal.innerHTML = `
+        <div class="modal-conteudo">
+            <button class="modal-fechar" onclick="fecharModal()">×</button>
+
+            <div class="modal-icone">!</div>
+
+            <h1>Campos incompletos</h1>
+
+            <p>Por favor, preencha todos os campos do registro para prosseguir.</p>
+
+            <p class="modal-registro">
+                Já possui cadastro?
+                <a href="../login.html">Fazer login</a>
+            </p>
+        </div>
+    `;
+
+    modal.classList.add("modal-aberto");
+}
+
+function fecharModal() {
+    document
+        .getElementById("modal_sem_usuario")
+        .classList.remove("modal-aberto");
+}
